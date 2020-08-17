029d7ad05a55ab68feaa4001441be1004eabbb56
Mr.Samet <s.burgazoglu@gmail.com>
2020-08-17 23:15:45 +0300

    Enemy taret hero tracker ability added

--- game_engine_lib/enemy_taret.py
+++ game_engine_lib/enemy_taret.py
@@ -2,7 +2,9 @@ from .npc import NPC
 from .skeleton import Skeleton
 from pygame import image
 from pygame.transform import rotate
+from pygame import draw
 from .tools import get_position_to_draw
+from math import sin, cos, radians
 
 
 class Enemy(NPC):
@@ -21,6 +23,12 @@ class Enemy(NPC):
         self.mana = 10
         self.pos_to_draw = (self.pos_x, self.pos_y)
 
+    def fire(self, display):
+        print(self.direction)
+        x2 = self.pos_x + cos(radians(self.direction)+90) * 200
+        y2 = self.pos_y + sin(radians(self.direction)+90) * 200
+        draw.line(display, (0, 0, 255), (self.pos_x, self.pos_y), (x2, y2), 20)
+
     def update(self, event, mouse_position, game_engine_lib):
         #drawer, scene, pause, display
         self.draw_immobile_character(self.current_image, game_engine_lib.display, self.pos_to_draw)
@@ -32,3 +40,4 @@ class Enemy(NPC):
                                         self.character_image.get_height()/2),
                                        self.direction)
             self.current_image = rotate(self.character_image, self.direction)
+            self.fire(game_engine_lib.display)
